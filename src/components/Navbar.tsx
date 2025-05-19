4a9fbb227eee0af88d14b1b993c260803143b8ea
@josearpaiaq <josearpaiaquintero@gmail.com>
2025-05-19 14:18:11 -0500

    refactor: update Navbar and LandingPage components for improved styling and text localization

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,17 +14,17 @@ export default function Navbar() {
   const { sidebar, setSidebar } = useChatterlyStore();
 
   return (
-    <header className="p-4 bg-gray-900/30">
+    <header className="p-4 bg-gray-900/50">
       <SignedOut>
         <div className="flex justify-end items-center gap-4">
           <SignInButton mode="modal">
-            <span className="text-sm font-bold cursor-pointer">
-              Inicia sesión
+            <span className="text-sm mt-2 font-bold cursor-pointer bg-amber-50 border-0 border-amber-500 rounded px-4 py-2 text-amber-950 hover:bg-amber-100 transition-all duration-300">
+              Sign In
             </span>
           </SignInButton>
           <SignUpButton mode="modal">
-            <span className="text-sm font-bold cursor-pointer">
-              Registrarse
+            <span className="text-sm mt-2 font-bold cursor-pointer bg-amber-50 border-0 border-amber-500 rounded px-4 py-2 text-amber-950 hover:bg-amber-100 transition-all duration-300">
+              Sign up
             </span>
           </SignUpButton>
         </div>
--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -5,12 +5,12 @@ export default function LandingPage() {
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">Chatterly</h1>
       <h2 className="text-2xl my-4">
-        Inicia sesión para usar Chatterly y empezar a hablar con tu IA.
+        Improve your english with AI and voice conversations
       </h2>
 
       <SignInButton mode="modal">
-        <span className="text-sm mt-2 font-bold cursor-pointer bg-amber-50 border-0 border-amber-500 rounded px-4 py-2 text-amber-950 ">
-          Iniciar sesión
+        <span className="text-sm mt-2 font-bold cursor-pointer bg-amber-50 border-0 border-amber-500 rounded px-4 py-2 text-amber-950 hover:bg-amber-100 transition-all duration-300">
+          Start your journey
         </span>
       </SignInButton>
     </div>
